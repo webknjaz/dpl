37472d065d9bdd588d69a38d0eb56451f03b9eba
Sviatoslav Sydorenko <wk@sydorenko.org.ua>
2017-02-22 13:55:03 +0200

    Enable pages provider to keep branch's history

--- lib/dpl/provider/pages.rb
+++ lib/dpl/provider/pages.rb
@@ -7,6 +7,7 @@ module DPL
         - repo [optional, for pushed to other repos]
         - github-token [required]
         - target-branch [optional, defaults to gh-pages]
+        - keep-history [optional, defaults to false]
         - local-dir [optional, defaults to `pwd`]
         - fqdn [optional]
         - project-name [optional, defaults to fqdn or repo slug]
@@ -29,9 +30,13 @@ module DPL
         @gh_ref = "github.com/#{slug}.git"
         @target_branch = options[:target_branch] || 'gh-pages'
         @gh_token = option(:github_token)
+        @keep_history = !!keep_history
 
         @gh_email = options[:email] || 'deploy@travis-ci.org'
         @gh_name = "#{options[:name] || 'Deployment Bot'} (from Travis CI)"
+
+        @gh_remote_url = "https://#{@gh_token}@#{@gh_ref}"
+        @git_push_opts = @keep_history ? '' : ' --force'
       end
 
       def fqdn
@@ -42,6 +47,10 @@ module DPL
         options.fetch(:repo) { context.env['TRAVIS_REPO_SLUG'] }
       end
 
+      def keep_history
+        options.fetch(:keep_history) { false }
+      end
+
       def check_auth
       end
 
@@ -49,22 +58,53 @@ module DPL
         false
       end
 
-      def github_deploy
-        context.shell 'rm -rf .git > /dev/null 2>&1'
-        context.shell "touch \"deployed at `date` by #{@gh_name}\""
-        context.shell 'git init' or raise 'Could not create new git repo'
+      def github_pull(target_dir)
+        unless context.shell "git clone --quiet --branch='#{@target_branch}' --single-branch '#{@gh_remote_url}' '#{target_dir}' &>/dev/null"
+          # if such branch doesn't exist at remote, do normal clone and create
+          # a new orphan branch
+          context.shell "git clone --quiet '#{@gh_remote_url}' '#{target_dir}' &>/dev/null"
+          context.shell "git checkout --orphan '#{@target_branch}'"
+        end
+      end
+
+      def github_clean
+        context.shell "git ls-files -z | xargs -0 rm -f"  # remove all committed files from the repo
+        context.shell "git ls-tree --name-only -d -r -z HEAD | sort -rz | xargs -0 rmdir"  # remove all directories from the repo
+      end
+
+      def github_init
+        context.shell 'rm -rf .git'
+        context.shell "git init" or raise 'Could not create new git repo'
+        context.shell "git checkout --orphan '#{@target_branch}'" or raise 'Could not create new git repo'
+      end
+
+      def github_configure
         context.shell "git config user.email '#{@gh_email}'"
         context.shell "git config user.name '#{@gh_name}'"
+      end
+
+      def github_deploy
+        context.shell "touch \"deployed at `date` by #{@gh_name}\""
         context.shell "echo '#{@gh_fqdn}' > CNAME" if @gh_fqdn
-        context.shell 'git add .'
+        context.shell 'git add -A .'
         context.shell "git commit -m 'Deploy #{@project_name} to #{@gh_ref}:#{@target_branch}'"
-        context.shell "git push --force --quiet 'https://#{@gh_token}@#{@gh_ref}' master:#{@target_branch} > /dev/null 2>&1"
+        context.shell "git push#{@git_push_opts} --quiet '#{@gh_remote_url}' '#{@target_branch}':'#{@target_branch}' &>/dev/null"
       end
 
       def push_app
         Dir.mktmpdir {|tmpdir|
+            if @keep_history
+              github_pull(tmpdir)
+              FileUtils.cd(tmpdir, :verbose => true) do
+                github_clean
+              end
+            end
             FileUtils.cp_r("#{@build_dir}/.", tmpdir)
             FileUtils.cd(tmpdir, :verbose => true) do
+              unless @keep_history
+                github_init
+              end
+              github_configure
               unless github_deploy
                 error "Couldn't push the build to #{@gh_ref}:#{@target_branch}"
               end
